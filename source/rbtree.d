bfba46df1c14c9bdd2a5b7b39a1429e8ce7bba9f
Ben Jones <ben.james.jones@gmail.com>
2025-11-25 21:54:25 -0700

    remove without rebalance implemented, starting on rebalance now

--- source/rbtree.d
+++ source/rbtree.d
@@ -2,6 +2,7 @@
 module rbtree;
 
 import std.stdio;
+import std.typecons : Tuple, tuple;
 
 /**
 Self balancing BST
@@ -48,6 +49,75 @@ class RBTree(T) {
         root.red = false; //make the root black.  This can't break anything
         return _size != oldSize;
     }
+
+
+    //if returns the node, as well as saying if the BH of this subtree
+    //shrunk (deleting can decrease it by 1)
+    private alias RemoveResult = Tuple!(Node*, "newRoot", bool, "bhChanged");
+
+    bool remove(T val){
+        //remove val from subtree rooted at n and return its new root
+        RemoveResult remove(Node* n, T val){
+            if(n !is null){
+                write("recursive remove ", val, " from subtree rooted at ");
+                writeln(n.data);
+            }
+
+            if(n is null){ return RemoveResult(null, false); }
+            if(n.data == val){
+                if(n.left == null){
+                    //TODO FIX RB properties
+                    _size--;
+                    return fixDelete(RemoveResult(n.right, !n.red)); //leaf or 1 child scenario
+                }
+                if(n.right == null){
+                    _size--;
+                    return fixDelete(RemoveResult(n.left, !n.red)); //1 child
+                } else {
+                    auto pred = n.left;
+                    while(pred.right !is null){ pred = pred.right; }
+                    n.data = pred.data;
+                    auto leftResult = remove(n.left, pred.data);
+                    n.left = leftResult.newRoot;
+                    return fixDelete(RemoveResult(n, leftResult.bhChanged));
+                }
+            }
+            if(val < n.data){
+                auto result = remove(n.left, val);
+                n.left = result.newRoot;
+                return fixDelete(RemoveResult(n, result.bhChanged));
+            } else {
+                auto result = remove(n.right, val);
+                n.right = result.newRoot;
+                return fixDelete(RemoveResult(n, result.bhChanged));
+            }
+        }
+        const oldSize = _size;
+        root = remove(root, val).newRoot;
+        if(root !is null){
+            root.red = false;  //make the root black
+        }
+        return _size != oldSize;
+    }
+
+
+    //fix any issues caused by removing a node from somewhere in this subtree
+    //we might have decreased the black height (n.bhChanged) or
+    //at some point caused 2 red nodes to be adjacent
+    //Note, even if we can detect a problem here, we might not fix it if
+    //the parent of n.newRoot needs to be changed to fix it
+    private RemoveResult fixDelete(RemoveResult n){
+        writeln("fixDelete of ", n.newRoot, " bh changed? ", n.bhChanged);
+        if(!n.bhChanged){
+            //only violaion can be Red/Red issue, which the fixInsert
+            //code already handles
+            return RemoveResult(fixInsert(n.newRoot), false);
+        } else {
+            assert(false, "bhChanged not implemented yet");
+
+        }
+    }
+
 
     //fix tree rooted at n and return the new root
     /*
@@ -158,32 +228,34 @@ class RBTree(T) {
             return;
         }
 
-        int blackHeight(Node* n){
-            if(n is null){ return 0; }
-            //            writeln("bh: ", n.data);
-
-            auto lh = blackHeight(n.left);
-            auto rh = blackHeight(n.right);
-            //            writeln("lh: ", lh, " rh: ", rh);
-            assert(lh == rh);
-            return lh + (n.red ? 0 : 1);
-
-        }
-
-        void checkRedRule(Node* n){
-            if(n is null) return;
-            if(n.red){
-                assert(n.left is null || !n.left.red);
-                assert(n.right is null || !n.right.red);
-            }
-            checkRedRule(n.left);
-            checkRedRule(n.right);
-
-        }
-
         blackHeight(root); //don't care what it is, but need to check the asserts
         checkRedRule(root);
     }
+
+    private int blackHeight(Node* n){
+        if(n is null){ return 0; }
+        writeln("bh: ", n.data);
+
+        auto lh = blackHeight(n.left);
+        auto rh = blackHeight(n.right);
+        writeln("lh: ", lh, " rh: ", rh);
+        assert(lh == rh);
+        return lh + (n.red ? 0 : 1);
+
+    }
+
+    private void checkRedRule(Node* n){
+        if(n is null) return;
+        if(n.red){
+            assert(n.left is null || !n.left.red);
+            assert(n.right is null || !n.right.red);
+        }
+        checkRedRule(n.left);
+        checkRedRule(n.right);
+
+    }
+
+
 
     private void printInOrder(Node* n){
         if(n is null) return;
@@ -192,10 +264,20 @@ class RBTree(T) {
         printInOrder(n.right);
     }
 
+    public void printInOrder(){
+
+        import std.conv: to;
+
+        writeln("tree root is: ", root.data);
+        writeln("root.left: ", root.left ? to!string(root.left.data) : "null",
+                " root.right: ", root.right ? to!string(root.right.data) : "null");
+        printInOrder(root);
+    }
+
 }
 
 
-unittest {
+void dunittest() {
 
     {
         scope tree = new RBTree!int();
@@ -244,7 +326,8 @@ unittest {
         import std.range: iota;
         import std.algorithm.iteration: permutations;
         writeln("checking all permutations of iota(10)");
-        foreach(perm; iota(10).permutations){
+        //TODO BUMP UP TO 10!
+        foreach(perm; iota(5).permutations){
             scope tree = new RBTree!int();
             foreach(x; perm){
                 assert(tree.add(x));
@@ -252,6 +335,23 @@ unittest {
                 assert(tree.contains(x));
             }
         }
+    }
+
+}
+
+//delete
+unittest {
+    import std.range : iota;
+    writeln("remove tests");
+    scope tree = new RBTree!int();
+    foreach(x; iota(10)){
+        tree.add(x);
+    }
+    foreach(x; iota(10)){
+        tree.printInOrder();
+        writeln("removing ", x);
+        assert(tree.remove(x));
+        tree.rbCheck();
     }
 
 }
